ac72f3a046547cea8732df52c2f48ea0a39760c6
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-28 04:09:47 -0500

    Update whitelist.py

--- modules/whitelist.py
+++ modules/whitelist.py
@@ -2,29 +2,51 @@ import os
 
 
 def Whitelist():
-    ip = input("Enter Your Personal IP Address: ")
-    print("Whitelisting IP Address!")
-    os.system(f"""iptables -I INPUT -p tcp --dport 22 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 22 -j DROP
-              iptables -I INPUT -p udp --dport 1194 -s {ip} -j ACCEPT
-              iptables -A INPUT -p udp --dport 1194 -j DROP
-              iptables -I INPUT -p tcp --dport 1194 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 1194 -j DROP
-              iptables -I INPUT -p tcp --dport 443 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 443 -j DROP
-              iptables -I INPUT -p udp --dport 443 -s {ip} -j ACCEPT
-              iptables -A INPUT -p udp --dport 443 -j DROP
-              iptables -I INPUT -p tcp --dport 80 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 80 -j DROP
-              iptables -I INPUT -p udp --dport 80 -s {ip} -j ACCEPT
-              iptables -A INPUT -p udp --dport 80 -j DROP
-              iptables -I INPUT -p tcp --dport 53 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 53 -j DROP
-              iptables -I INPUT -p udp --dport 53 -s {ip} -j ACCEPT
-              iptables -A INPUT -p udp --dport 53 -j DROP
-              iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
-              iptables -A INPUT -p udp --dport 21 -j DROP
-              iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
-              iptables -A INPUT -p tcp --dport 21 -j DROP
-              iptables-save > rules.v4
-            """)
+    print("""    1. Whitelist Server
+    2. Whitelist Server And Web""")
+    choices = input("Select A Choice: ")
+    if choices == "1":
+        ip = input("Enter Your Personal IP Address: ")
+        print("Whitelisting Server!")
+        os.system(f"""iptables -I INPUT -p tcp --dport 22 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 22 -j DROP
+                  iptables -I INPUT -p udp --dport 1194 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 1194 -j DROP
+                  iptables -I INPUT -p tcp --dport 1194 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 1194 -j DROP
+                  iptables -A INPUT -p tcp --dport 53 -j DROP
+                  iptables -I INPUT -p udp --dport 53 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 53 -j DROP
+                  iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 21 -j DROP
+                  iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 21 -j DROP
+                  iptables -I INPUT -p udp --dport 80 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 80 -j DROP
+                  iptables-save > rules.v4""")
+    elif choices == "2":
+        ip = input("Enter Your Personal IP Address: ")
+        print("Whitelisting Server And Web!")
+        os.system("""iptables -I INPUT -p tcp --dport 443 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 443 -j DROP
+                  iptables -I INPUT -p udp --dport 443 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 443 -j DROP
+                  iptables -I INPUT -p tcp --dport 80 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 80 -j DROP
+                  iptables -I INPUT -p udp --dport 80 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 80 -j DROP
+                  iptables -I INPUT -p tcp --dport 53 -s {ip} -j ACCEPT
+                  iptables -I INPUT -p tcp --dport 22 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 22 -j DROP
+                  iptables -I INPUT -p udp --dport 1194 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 1194 -j DROP
+                  iptables -I INPUT -p tcp --dport 1194 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 1194 -j DROP
+                  iptables -A INPUT -p tcp --dport 53 -j DROP
+                  iptables -I INPUT -p udp --dport 53 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 53 -j DROP
+                  iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p udp --dport 21 -j DROP
+                  iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+                  iptables -A INPUT -p tcp --dport 21 -j DROP
+                  iptables-save > rules.v4""")
