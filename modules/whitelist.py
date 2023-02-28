e0834716424035e433cbb457eb33b753b24cf3f3
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-28 04:01:44 -0500

    Update whitelist.py

--- modules/whitelist.py
+++ modules/whitelist.py
@@ -2,6 +2,29 @@ import os
 
 
 def Whitelist():
-    os.system("""echo test
-              echo 2
-              echo 3""")
+    ip = input("Enter Your Personal IP Address: ")
+    print("Whitelisting IP Address!")
+    os.system(f"""iptables -I INPUT -p tcp --dport 22 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 22 -j DROP
+              iptables -I INPUT -p udp --dport 1194 -s {ip} -j ACCEPT
+              iptables -A INPUT -p udp --dport 1194 -j DROP
+              iptables -I INPUT -p tcp --dport 1194 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 1194 -j DROP
+              iptables -I INPUT -p tcp --dport 443 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 443 -j DROP
+              iptables -I INPUT -p udp --dport 443 -s {ip} -j ACCEPT
+              iptables -A INPUT -p udp --dport 443 -j DROP
+              iptables -I INPUT -p tcp --dport 80 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 80 -j DROP
+              iptables -I INPUT -p udp --dport 80 -s {ip} -j ACCEPT
+              iptables -A INPUT -p udp --dport 80 -j DROP
+              iptables -I INPUT -p tcp --dport 53 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 53 -j DROP
+              iptables -I INPUT -p udp --dport 53 -s {ip} -j ACCEPT
+              iptables -A INPUT -p udp --dport 53 -j DROP
+              iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+              iptables -A INPUT -p udp --dport 21 -j DROP
+              iptables -I INPUT -p tcp --dport 21 -s {ip} -j ACCEPT
+              iptables -A INPUT -p tcp --dport 21 -j DROP
+              iptables-save > rules.v4
+            """)
