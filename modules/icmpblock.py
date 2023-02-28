3f54b13ef23e6640befb2e2516f566915788870e
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-28 04:29:08 -0500

    Update icmpblock.py

--- modules/icmpblock.py
+++ modules/icmpblock.py
@@ -3,4 +3,5 @@ import os
 def block():
     print("Blocking ICMP")
     os.system("""sudo iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
-              sudo iptables -A OUTPUT -p icmp --icmp-type echo-reply -j DROP""")
+              sudo iptables -A OUTPUT -p icmp --icmp-type echo-reply -j DROP
+              iptables-save > rules.v4""")
