05ef8f8e70d93fdffdaa51b38e61d136082375f7
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-28 03:15:36 -0500

    Update ratelimit.py

--- modules/ratelimit.py
+++ modules/ratelimit.py
@@ -3,6 +3,7 @@ import os
 def ratelimit():
     print("Rate Limiting tcp connections")
     #Rate Limiting TCP
+    os.system("ufw limit 1:65535/tcp")
     os.system("iptables -t mangle -I PREROUTING -p tcp -m conntrack --ctstate NEW -j DROP")
     os.system("iptables -t mangle -I PREROUTING -p tcp -m conntrack --ctstate NEW -m hashlimit --hashlimit-name portlimit --hashlimit-mode srcip --hashlimit-srcmask 32 --hashlimit-upto 23/sec --hashlimit-burst 30 --hashlimit-htable-expire 60000 -j ACCEPT")
     #Rate Limiting UDP
