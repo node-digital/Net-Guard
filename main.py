67e725bf5863c803ead9fed3a75a46f7af963ddd
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2023-02-28 03:02:28 -0500

    Initial Release With Ratelimit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os, sys
+
+def Clear():
+    os.system("clear")
+
+banner = """Welcome To NetGuard - V0.2
+1. Rate Limiting
+2. Exit"""
+
+banner()
+choice = input("Choose An Option: ")
+
+if choice == "1":
+    import modules.ratelimit as ratelimit
+    ratelimit.ratelimit()
+
+if choice == "2":
+    sys.exit()
