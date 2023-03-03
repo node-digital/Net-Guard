785df4cd7ce0afbb51598ea0af62464a7fe8288b
qqa112811 <126834653+qqa112811@users.noreply.github.com>
2023-03-02 19:31:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,28 @@ def print_banner():
     7. Exit"""
     print(banner)
 
+def handle_rate_limit():
+    interface = input("Enter the network interface to rate limit (e.g., eth0): ")
+    rate = input("Enter the rate limit in kilobits per second (e.g., 1000): ")
+    ratelimit.rate_limit(interface, rate)
+
+def handle_ip_forward():
+    src_ip = input("Enter the source IP address to forward (e.g., 192.168.1.1): ")
+    dst_ip = input("Enter the destination IP address to forward (e.g., 10.0.0.1): ")
+    port = input("Enter the port to forward (e.g., 80): ")
+    ipforward.ip_forward(src_ip, dst_ip, port)
+
 def handle_choice(choice):
     if choice == "1":
-        ratelimit.limit()
+        handle_rate_limit()
     elif choice == "2":
         whitelist.Whitelist()
     elif choice == "3":
         asn.asnblock()
     elif choice == "4":
-        ipforward.forward()
+        handle_ip_forward()
     elif choice == "5":
-        icmp.block()
+        icmp.icmp_block()
     elif choice == "6":
         port.drop()
     elif choice == "7":
