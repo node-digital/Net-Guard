6b9be2baff8f7e2c80797ad4ef137fa1b490fa9e
qqa112811 <126834653+qqa112811@users.noreply.github.com>
2023-03-02 19:21:41 -0500

    Update ipforward.py

--- modules/ipforward.py
+++ modules/ipforward.py
@@ -3,4 +3,3 @@ import os
 def ip_forward(source_ip, destination_ip, port):
     os.system("sudo iptables -t nat -A PREROUTING -p tcp -d {source_ip} --dport {port} -j DNAT --to-destination {destination_ip}:{port}")
     os.system("sudo iptables -t nat -A POSTROUTING -j MASQUERADE")
-    print("IP forwarding set up successfully.")
